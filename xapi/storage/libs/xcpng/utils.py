04d1e27cb6c815bde60a11af7aa0ffae8bead7f1
Roman Posudnevskiy <ramzes_r@yahoo.com>
2019-02-07 11:04:49 +0300

    Fixes due to changed uri structure

--- xapi/storage/libs/xcpng/utils.py
+++ xapi/storage/libs/xcpng/utils.py
@@ -19,7 +19,8 @@ MIN_VHD_SIZE = 2 * 1024 * 1024
 MAX_VHD_SIZE = 2093050 * 1024 * 1024
 VHD_BLOCK_SIZE = 2 * 1024 * 1024
 RBD_BLOCK_SIZE = 2 * 1024 * 1024
-RBD_BLOCK_ORDER = int(math.log(RBD_BLOCK_SIZE,2))
+RBD_BLOCK_ORDER = int(math.log(RBD_BLOCK_SIZE, 2))
+
 
 def get_vdi_type_by_uri(dbg, uri):
     scheme = urlparse.urlparse(uri).scheme
@@ -27,10 +28,6 @@ def get_vdi_type_by_uri(dbg, uri):
     result = regex.match(scheme)
     return result.group(2)
 
-def subst_vdi_type_in_uri(dbg, uri, type):
-    regex = re.compile('(.*)\+(.*)\+(.*)')
-    result = regex.match(uri)
-    return "%s+%s+%s" % (result.group(1), type, result.group(3))
 
 def get_vdi_datapath_by_uri(dbg, uri):
     scheme = urlparse.urlparse(uri).scheme
@@ -38,35 +35,48 @@ def get_vdi_datapath_by_uri(dbg, uri):
     result = regex.match(scheme)
     return result.group(3)
 
+
 def get_cluster_name_by_uri(dbg, uri):
     return urlparse.urlparse(uri).netloc
 
+
 def get_sr_uuid_by_uri(dbg, uri):
-    return urlparse.urlparse(uri).netloc
+    regex = re.compile('/([A-Za-z0-9\-]*)/{0,1}([A-Za-z0-9\-]*)')
+    result = regex.match(urlparse.urlparse(uri).path)
+    return result.group(1)
+
 
 def get_sr_uuid_by_name(dbg, name):
     regex = re.compile(".*%s" % POOL_PREFIX)
     return regex.sub('', name)
 
+
 def get_vdi_uuid_by_uri(dbg, uri):
-    return urlparse.urlparse(uri).path[1:]
+    regex = re.compile('/([A-Za-z0-9\-]*)/{0,1}([A-Za-z0-9\-]*)')
+    result = regex.match(urlparse.urlparse(uri).path)
+    return result.group(2)
+
 
 def get_vdi_uuid_by_name(dbg, name):
     regex = re.compile('.*-(.{8}-.{4}-.{4}-.{4}-.{12})')
     result = regex.match(name)
     return result.group(1)
 
-def get_pool_name_by_uri(dbg, uri):
+
+def get_sr_name_by_uri(dbg, uri):
     return "%s%s%s" % (get_sr_type_by_uri(dbg, uri), POOL_PREFIX, get_sr_uuid_by_uri(dbg, uri))
 
-def get_image_name_by_uri(dbg, uri):
+
+def get_vdi_name_by_uri(dbg, uri):
     return "%s%s" % (VDI_PREFIXES[get_vdi_type_by_uri(dbg, uri)], get_vdi_uuid_by_uri(dbg, uri))
+
 
 def get_sr_type_by_uri(dbg, uri):
     scheme = urlparse.urlparse(uri).scheme
-    regex = re.compile('(.*)\+(.*)\+(.*)')
+    regex = re.compile('^(\w*)\+{0,1}(\w*)\+{0,1}(\w*)$')
     result = regex.match(scheme)
     return result.group(1).upper()
+
 
 def get_current_host_uuid():
     fd = open("/etc/xensource-inventory")
@@ -75,21 +85,38 @@ def get_current_host_uuid():
             return line.split("'")[1]
     fd.close()
 
+
+def get_known_srs():
+    srs = []
+    args = ['xe', 'sr-list']
+    proc = Popen(args, stdout=PIPE, close_fds=True)
+    stdout = proc.communicate()[0]
+    if proc.returncode == 0:
+        for line in stdout.split('\n'):
+            if 'uuid' in line:
+                srs.append(line.split(':')[1].strip())
+    return srs
+
+
 def call(dbg, args):
     log.debug("%s: Running cmd: %s" % (dbg, args))
     proc = Popen(args, stdout=PIPE, stderr=PIPE, close_fds=True)
     stdout, stderr = proc.communicate()
     if proc.returncode != 0:
-        log.debug("%s: Exec of cmd: %s failed, error: %s, reason: %s" % (dbg, args, os.strerror(proc.returncode), stderr.strip()))
+        log.debug("%s: Exec of cmd: %s failed, error: %s, reason: %s"
+                  % (dbg, args, os.strerror(proc.returncode), stderr.strip()))
         raise Exception(os.strerror(proc.returncode))
     return stdout
+
 
 def _call(dbg, args):
     log.debug("%s: Running cmd: %s" % (dbg, args))
     proc = Popen(args, stdout=PIPE, stderr=PIPE, close_fds=True)
     stdout, stderr = proc.communicate()
-    log.debug("%s: Cmd return: %s, error: %s, reason: %s" % (dbg, proc.returncode, os.strerror(proc.returncode), stderr.strip()))
+    log.debug("%s: Cmd return: %s, error: %s, reason: %s"
+              % (dbg, proc.returncode, os.strerror(proc.returncode), stderr.strip()))
     return proc.returncode
+
 
 def mkdir_p(path, mode=None):
     if not mode:
@@ -103,12 +130,13 @@ def mkdir_p(path, mode=None):
         except OSError:
             raise
 
+
 # Functions from /opt/xensource/sm/vhdutil.py
 
 def calcOverheadEmpty(virtual_size):
     """Calculate the VHD space overhead (metadata size) for an empty VDI of
     size virtual_size"""
-    overhead = 0
+
     size_mb = virtual_size / (1024 * 1024)
 
     # Footer + footer copy + header + possible CoW parent locator fields
@@ -124,19 +152,23 @@ def calcOverheadEmpty(virtual_size):
 
     return overhead
 
+
 def calcOverheadBitmap(virtual_size):
     num_blocks = virtual_size / VHD_BLOCK_SIZE
     if virtual_size % VHD_BLOCK_SIZE:
         num_blocks += 1
     return num_blocks * 4096
 
+
 def calcOverheadFull(virtual_size):
     """Calculate the VHD space overhead for a full VDI of size virtual_size
     (this includes bitmaps, which constitute the bulk of the overhead)"""
     return calcOverheadEmpty(virtual_size) + calcOverheadBitmap(virtual_size)
 
+
 def fullSizeVHD(virtual_size):
     return virtual_size + calcOverheadFull(virtual_size)
+
 
 def roundup(divisor, value):
     """Retruns the rounded up value so it is divisible by divisor."""
@@ -146,16 +178,17 @@ def roundup(divisor, value):
         return ((int(value) / divisor) + 1) * divisor
     return value
 
+
 def validate_and_round_vhd_size(size):
     """ Take the supplied vhd size, in bytes, and check it is positive and less
     that the maximum supported size, rounding up to the next block boundary
     """
-    if (size < 0 or size > MAX_VHD_SIZE):
+    if size < 0 or size > MAX_VHD_SIZE:
         raise xapi.XenAPIException('VDISize',
                                    ['VDI size must be between %d MB and %d MB' %
-                                   ((MIN_VHD_SIZE / 1024 / 1024), (MAX_VHD_SIZE / 1024 / 1024))])
+                                    ((MIN_VHD_SIZE / 1024 / 1024), (MAX_VHD_SIZE / 1024 / 1024))])
 
-    if (size < MIN_VHD_SIZE):
+    if size < MIN_VHD_SIZE:
         size = MIN_VHD_SIZE
 
     size = roundup(VHD_BLOCK_SIZE, size)
