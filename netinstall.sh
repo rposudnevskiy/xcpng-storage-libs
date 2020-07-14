f132774bce5188f68cca6b7e03f4eb2efeb8673d
Roman Posudnevskiy <ramzes_r@yahoo.com>
2020-07-14 23:52:52 +0300

    Make unzip overwrite files without prompt

--- netinstall.sh
+++ netinstall.sh
@@ -5,7 +5,7 @@ BRANCH="master"
 
 cd ~
 wget -q "$REPO/$PROJECT/archive/$BRANCH.zip" -O ~/$PROJECT-temp.zip
-unzip -qq ~/$PROJECT-temp.zip -d ~
+unzip -qq -o ~/$PROJECT-temp.zip -d ~
 cd ~/$PROJECT-$BRANCH
 
 sh ./install/$PROJECT.sh install $1
