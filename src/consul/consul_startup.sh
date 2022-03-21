a3bb2d1887bf4628cbefb4f0f9d23f342f35274b
Roman Posudnevskiy <roman.posudnevskiy@gmail.com>
2022-03-21 15:28:54 +0300

    Fixed consul_startup/sh script
    - fixed http_addr param to connect to pool master retrieved from xapi

--- src/consul/consul_startup.sh
+++ src/consul/consul_startup.sh
@@ -2,11 +2,10 @@
 if [[ `cat /etc/xensource/pool.conf` = 'master' ]]; then
         /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -server -bootstrap
 else
-        if [[ `consul members -http-addr="http://192.168.57.200:8500" | grep server | wc -l` -le 1 ]]; then
-                /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -server -retry-join \
-                    \"`xe host-param-get param-name=address uuid=\`xe pool-list | grep master | awk '{print $4}'\``\"
+        pool_master=`xe host-param-get param-name=address uuid=\`xe pool-list | grep master | awk '{print $4}'\``
+        if [[ `consul members -http-addr="http://$pool_master:8500" | grep server | wc -l` -le 1 ]]; then
+                /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -server -retry-join "$pool_master"
         else
-                /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -retry-join \
-                    \"`xe host-param-get param-name=address uuid=\`xe pool-list | grep master | awk '{print $4}'\``\"
+                /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -retry-join "$pool_master"
         fi
 fi
