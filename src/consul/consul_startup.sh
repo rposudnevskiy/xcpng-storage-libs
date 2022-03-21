1f3c6cf013344631388eb707fa87097de360c929
Roman Posudnevskiy <roman.posudnevskiy@gmail.com>
2022-03-22 00:02:51 +0300

    Retrieve pool master IP from /etc/xensource/pool.conf

--- src/consul/consul_startup.sh
+++ src/consul/consul_startup.sh
@@ -2,8 +2,8 @@
 if [[ `cat /etc/xensource/pool.conf` = 'master' ]]; then
         /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -server -bootstrap
 else
-        pool_master=`xe host-param-get param-name=address uuid=\`xe pool-list | grep master | awk '{print $4}'\``
-        if [[ `consul members -http-addr="http://$pool_master:8500" | grep server | wc -l` -le 1 ]]; then
+        pool_master=`awk -F: '{print $2}' /etc/xensource/pool.conf`
+        if [[ `consul members -http-addr="http://$pool_master:8500" | grep server | wc -l` -le 5 ]]; then
                 /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -server -retry-join "$pool_master"
         else
                 /usr/local/bin/consul agent -config-dir=/etc/consul.d/ -retry-join "$pool_master"
